a3e69f9e6feca6297a6c670b688df6cf0d03be49
dell <dell@DESKTOP-4P8U8AR>
2024-04-08 20:43:51 +0000

    TeacherRepository Desing :)

--- src/main/java/repository/TeacherRepository.java
+++ src/main/java/repository/TeacherRepository.java
@@ -1,5 +1,18 @@
 package repository;
 
-public class TeacherRepository {
+import java.util.List;
 
+import model.Teacher;
+
+public interface TeacherRepository {
+
+	Teacher save(Teacher teacher);
+
+    Teacher findById(int id);
+
+    List<Teacher> findAll();
+
+    Teacher update(Teacher teacher);
+
+    void deleteById(int id);
 }
