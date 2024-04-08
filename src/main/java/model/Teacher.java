6bcf53b113a4ae6a39c4794ccb7671b3014565fa
dell <dell@DESKTOP-4P8U8AR>
2024-04-08 12:58:25 +0000

    Models : Teachers

--- src/main/java/model/Teacher.java
+++ src/main/java/model/Teacher.java
@@ -2,4 +2,65 @@ package model;
 
 public class Teacher {
 
+	private int id;
+	private String firstName;
+	private String lastName;
+	private String subject;
+	private String email;
+	
+	public Teacher() {
+		super();
+	}
+
+	public Teacher(int id, String firstName, String lastName, String subject, String email) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.subject = subject;
+		this.email = email;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	
+	
 }
